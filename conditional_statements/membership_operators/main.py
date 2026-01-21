28dbc49af3d7b8d3d869775ae61cdcab4fc0c887
Zammy97 <>
2026-01-21 13:30:53 +0000

    Membership Operators

---
+++ conditional_statements/membership_operators/main.py
@@ -0,0 +1,13 @@
+# Product details
+description = "Imported honey, raw and unfiltered"
+price = "5.99"
+count = 120
+
+# Write your code here
+contains_raw = "raw" in description
+print(contains_raw)
+contains_Imported = "Imported" in description
+print(contains_Imported)
+
+print("Contains 'raw':", contains_raw)
+print("Contains 'Imported':", contains_Imported)
